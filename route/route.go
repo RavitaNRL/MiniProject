1d9ff5bfca3eef0030bd6ffd5b3477368bb6aaf2
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-05-05 15:56:29 +0700

    update route

--- route/route.go
+++ route/route.go
@@ -1,17 +1,37 @@
 package route
 
 import (
+	"MiniProject/constants"
 	"MiniProject/controller"
+	"MiniProject/middleware"
 
 	"github.com/labstack/echo"
+	mid "github.com/labstack/echo/middleware"
 )
 
 func New() *echo.Echo {
+	// Echo instance
 	e := echo.New()
 
-	e.GET("/order", controller.GetOrderById)
-	e.POST("/order", controller.CreateOrder)
+	// Middleware
+	middleware.LogMiddleware(e)
+	e.Pre(mid.RemoveTrailingSlash())
+
+	// User Controllers
+	user := e.Group("/user")
+	user.POST("", controller.CreateUser)
+	user.POST("/login", controller.LoginUsers)
+	user.GET("", controller.GetAllUser, mid.JWT([]byte(constants.SCREAT_JWT)))
+	user.GET("/:id", controller.GetUserByID, mid.JWT([]byte(constants.SCREAT_JWT)))
+	user.PUT("/:id", controller.UpdateUser, mid.JWT([]byte(constants.SCREAT_JWT)))
+	user.DELETE("/:id", controller.DeleteUser, mid.JWT([]byte(constants.SCREAT_JWT)))
+
+	// Baju Controllers
+	baju := e.Group("/baju")
+	baju.POST("", controller.CreateBaju, mid.JWT([]byte(constants.SCREAT_JWT)))
+	baju.GET("", controller.GetAllBaju, mid.JWT([]byte(constants.SCREAT_JWT)))
+	baju.GET("/:id", controller.GetBajuByID, mid.JWT([]byte(constants.SCREAT_JWT)))
+	baju.PUT("/:id", controller.UpdateBaju, mid.JWT([]byte(constants.SCREAT_JWT)))
 
 	return e
-
 }
