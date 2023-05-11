cfdcab12d9cf060deeb0e338cd43f3ff27ee59f4
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-05-11 11:02:42 +0700

    update database pada config

--- config/config.go
+++ config/config.go
@@ -1,16 +1,16 @@
 package config
 
 import (
+	"Project-Mini/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-
-	"miniProject/models"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "root:@tcp(localhost:3306)/mini-project?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(localhost:3306)/project_mini?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
